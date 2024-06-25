a6e914c3c880d053c06115abd59f8bce3b6bd458
Anubhav Yadav <ay6666@srmist.edu.in>
2024-06-25 20:47:05 +0530

    Build URL Dynamically

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 
 ## WSGI Application
 app = Flask(__name__)
@@ -10,6 +10,27 @@ def welcome():
 @app.route('/model')
 def ml_model():
     return "This is a machine learning model."
+
+#### Building URL Dynamically ####
+# Variable Rules and URL Building
+@app.route('/success/<int:score>')
+def success(score):
+    return "The student has acheived marks "+ str(score)
+
+@app.route('/fail/<int:score>')
+def failed(score):
+    return "The student has failed with marks "+ str(score)
+
+# Result Checker
+@app.route('/results/<int:marks>')
+def results(marks):
+    result=''
+    if marks<50:
+        result = 'failed'
+    else:
+        result = 'success'
+    
+    return redirect(url_for(result, score=marks))
 
 if __name__ == '__main__':
     app.run(debug=True)
